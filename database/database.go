e7337cec1aa3f97ba69c78b770059267c3f62795
Siratee K <sirateek@gmail.com>
2023-04-16 23:23:55 +0700

    INitial Commit

---
+++ database/database.go
@@ -0,0 +1,11 @@
+package database
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func Conn(connectionString string) (*gorm.DB, error) {
+	// Start DB Connection
+	return gorm.Open(mysql.Open(connectionString))
+}
