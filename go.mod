1067fcf94774d720467fbf6fd65eada633fbfdba
Siratee K <sirateek@gmail.com>
2023-04-17 14:20:55 +0700

    fix: Make httpserver allow CORs policy

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/GarnBarn/common-go
 go 1.20
 
 require (
+	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.0
 	github.com/sirupsen/logrus v1.9.0
 	gorm.io/driver/mysql v1.5.0
--- httpserver/httpserver.go
+++ httpserver/httpserver.go
@@ -1,11 +1,29 @@
 package httpserver
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func NewHttpServer() *gin.Engine {
 	router := gin.Default()
+
+	// Allow CORs Policy
+	// TODO: Set the appropriate permission
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"*"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return true
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
 	router.GET("/healthcheck", healthCheckHandler)
 	return router
 }
