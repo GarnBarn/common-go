6f14c7164e7005673adfcca3f189f9bb1cf2cff3
Siratee K <sirateek@gmail.com>
2023-04-18 22:43:41 +0700

    feat: Add rabbitmq wrapper

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.0
 	github.com/sirupsen/logrus v1.9.0
+	github.com/wagslane/go-rabbitmq v0.12.3
 	google.golang.org/grpc v1.54.0
 	google.golang.org/protobuf v1.28.1
 	gorm.io/driver/mysql v1.5.0
@@ -31,6 +32,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/rabbitmq/amqp091-go v1.7.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
---
+++ rabbitmq/rabbitmq.go
@@ -0,0 +1,110 @@
+package rabbitmq
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/sirupsen/logrus"
+	"github.com/wagslane/go-rabbitmq"
+)
+
+type ConsumerConfig struct {
+	ConnectionString   string
+	MaxRetry           int
+	DeadLetterExchange string
+	FailoverExchange   string
+	ConsumeQueue       string
+}
+
+type Processor interface {
+	Process(d rabbitmq.Delivery) error
+}
+
+type rabbitMQ struct {
+	conn      *rabbitmq.Conn
+	publisher *rabbitmq.Publisher
+}
+
+type RabbitMQ interface {
+	Consume(processor Processor, consumerConfig ConsumerConfig) (*rabbitmq.Consumer, error)
+	GetPublisher() *rabbitmq.Publisher
+}
+
+func NewRabbitMQ(connectionString string) (RabbitMQ, error) {
+	conn, err := rabbitmq.NewConn(
+		connectionString,
+		rabbitmq.WithConnectionOptionsLogging,
+	)
+	if err != nil {
+		logrus.Error("RabbitMQ Error: ", err)
+		return &rabbitMQ{}, err
+	}
+
+	publisher, err := rabbitmq.NewPublisher(
+		conn,
+		rabbitmq.WithPublisherOptionsLogging,
+	)
+	if err != nil {
+		logrus.Error("RabbitMQ Error: ", err)
+		return &rabbitMQ{}, err
+	}
+
+	return &rabbitMQ{
+		conn:      conn,
+		publisher: publisher,
+	}, nil
+}
+
+func (r *rabbitMQ) GetPublisher() *rabbitmq.Publisher {
+	return r.publisher
+}
+
+func (r *rabbitMQ) Consume(processor Processor, consumerConfig ConsumerConfig) (*rabbitmq.Consumer, error) {
+	return rabbitmq.NewConsumer(
+		r.conn,
+		func(d rabbitmq.Delivery) (action rabbitmq.Action) {
+			logrus.Info("Start Processing the message")
+			defer logrus.Info("End Processing the message")
+			err := processor.Process(d)
+
+			if err != nil {
+				value, ok := d.Headers["x-retry"]
+				retryCount := 0
+				if ok {
+					convertResult, err := strconv.Atoi(fmt.Sprint(value))
+					if err != nil {
+						convertResult = 0
+					}
+					retryCount = convertResult
+				}
+
+				retryCount++
+
+				rabbitMqHeaderTable := rabbitmq.Table{
+					"x-retry": retryCount,
+				}
+
+				if retryCount >= consumerConfig.MaxRetry {
+					logrus.Warn("Maximum retry exceeded, Publishing the message to dead lettering exchange")
+					r.publisher.Publish(d.Body, []string{d.RoutingKey},
+						rabbitmq.WithPublishOptionsExchange(consumerConfig.DeadLetterExchange),
+						rabbitmq.WithPublishOptionsContentType(d.ContentType),
+						rabbitmq.WithPublishOptionsHeaders(rabbitMqHeaderTable),
+					)
+					return rabbitmq.Ack
+				}
+
+				logrus.Warn("Publishing message back to exchange, ")
+				r.publisher.Publish(d.Body, []string{d.RoutingKey},
+					rabbitmq.WithPublishOptionsExchange(consumerConfig.FailoverExchange),
+					rabbitmq.WithPublishOptionsContentType(d.ContentType),
+					rabbitmq.WithPublishOptionsHeaders(rabbitMqHeaderTable),
+				)
+			}
+
+			return rabbitmq.Ack
+		},
+		consumerConfig.ConsumeQueue,
+		rabbitmq.WithConsumerOptionsQueueDurable,
+	)
+}
