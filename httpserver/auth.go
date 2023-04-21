f93655dcba0a06857ab0cd5df377368922ea8862
Siratee K <sirateek@gmail.com>
2023-04-21 21:23:27 +0700

    feat: Add Middleware to map the auth value into the header

---
+++ httpserver/auth.go
@@ -0,0 +1,34 @@
+package httpserver
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthModelMapping() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		key := c.GetHeader(UserUidKey)
+		splittedKey := strings.Split(key, " ")
+		tokenStr := splittedKey[1]
+
+		token, err := jwt.Parse(tokenStr, nil)
+		if token == nil || err != nil {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+		claims, _ := token.Claims.(jwt.MapClaims)
+
+		result, ok := claims["user_id"].(string)
+
+		if !ok {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		c.Header(UserUidKey, result)
+		c.Next()
+	}
+}
